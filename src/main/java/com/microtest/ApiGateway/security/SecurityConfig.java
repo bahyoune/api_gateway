acaa9b12c8d13ec1474fa05aea38ede91bfcfd3a
bahyoune <younetahara@gmail.com>
2026-02-24 18:51:43 +0000

    Clean Code + APIs updated version 2

--- src/main/java/com/microtest/ApiGateway/security/SecurityConfig.java
+++ src/main/java/com/microtest/ApiGateway/security/SecurityConfig.java
@@ -6,13 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.server.WebFilter;
 import reactor.core.publisher.Mono;
 
 
@@ -27,9 +25,9 @@ public class SecurityConfig {
                 .authorizeExchange(auth -> auth
                         .pathMatchers("/actuator/**").permitAll()
                         .pathMatchers("/auth/**").permitAll()
-                        .pathMatchers("/api/v1/payment/**").hasRole("ADMIN")
-                        .pathMatchers("/api/v1/orders/**").hasAnyRole("USER", "ADMIN")
-                        .anyExchange().authenticated()
+                        .pathMatchers("/internal/v1/payment/**").hasRole("ADMIN")
+                        .pathMatchers("/internal/v1/orders/**").hasAnyRole("USER", "ADMIN")
+                        .anyExchange().denyAll()
                 )
                 // Recommended path: use Spring's resource server to validate JWT from Authorization header
                 .oauth2ResourceServer(oauth2 ->
